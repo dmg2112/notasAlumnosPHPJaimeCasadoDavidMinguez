6c700a55f4ba2cf69edb8f572eb66c403673e2e9
jaime casado <jaime.casado.a@gmail.com>
2019-12-02 13:42:12 +0100

    Menu

--- Menu.php
+++ Menu.php
@@ -1,6 +1,26 @@
-<?php
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+    <title>Menu</title>
+  </head>
+  <body>
+    <div class="container">
+	<div class="row flex-row p-5 m-5">
+		<a href="./MostrarAlumnos.php" class="col-6">
+			<h1 class="text-center">Mostrar notas por alumnos</h1>
+		</a>
+		<a href="./MostrarAsignaturas.php" class="col-6">
+			<h1 class="text-center">Mostrar notas por asignatura</h1>
+		</a>
+   	</div>
+    </div>
 
-
-?>
+  </body>
+</html>
