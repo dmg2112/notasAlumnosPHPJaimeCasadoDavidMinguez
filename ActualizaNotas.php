b6ee0441c140a3d310e9b68f44fda23686e14d84
jaime casado <jaime.casado.a@gmail.com>
2019-12-09 16:31:00 +0100

    Borrar y annadir finalizados para alumnos

--- ActualizaNotas.php
+++ ActualizaNotas.php
@@ -1,44 +1,47 @@
 <?php
+require 'vendor/autoload.php'; // incluir lo bueno de Composer
+$cliente = new MongoDB\client;
+$alumnos = $cliente->ADAT_UD3_A04->alumnos;
 
-    if(isset($_GET['matricula'])){
-        if(isset($_GET['tarea']) && isset($_GET['nota']) && isset($_GET['asignatura'])){
-		
-		
-            require 'vendor/autoload.php'; // incluir lo bueno de Composer
-            $cliente = new MongoDB\client;
-            $matr = strval($_GET['matricula']);
-            $tarea = intval($_GET['tarea']);
-            $nota = intval($_GET['nota']);
-            $asig = $_GET['asignatura'];
-    
-            $alumnos = $cliente->ADAT_UD3_A04->alumnos;
+if (isset($_GET['matricula'])) {
 
-            
-            
-            $updateResult = $alumnos->updateOne(
-                [ 'nMatricula' => $matr,
-            'notasAlumno.codigoAsignatura'=> $asig ],
-                [ '$pull' => [ 'notasAlumno.$.notasAsignatura' => ['numTarea'=> $tarea] ]]
-    
-            );
-            $updateResult = $alumnos->updateOne(
-                [ 'nMatricula' => $matr,
-            'notasAlumno.codigoAsignatura'=> $asig ],
-                [ '$push' => [ 'notasAlumno.$.notasAsignatura' => ['numTarea'=> $tarea , 'notaTarea'=> $nota] ]]
-    
-            );
+    if (isset($_GET['tarea']) && isset($_GET['nota']) && isset($_GET['asignatura'])) {
 
+        $matr = strval($_GET['matricula']);
+        $tarea = intval($_GET['tarea']);
+        $nota = intval($_GET['nota']);
+        $asig = $_GET['asignatura'];
 
+        pull($alumnos, $matr, $asig, $tarea);
 
-            header("Location: ./DetalleAlumno.php?matricula=".$_GET['matricula']);
-            
-        }else{
-            header("Location: ./DetalleAlumno.php?matricula=".$_GET['matricula']);
-        }
-
-    }else{
-        header("Location: ./Menu.php");
+        push($alumnos, $matr, $asig, $tarea, $nota);
+    } else if (isset($_GET['tarea']) && isset($_GET['asignatura'])) {
+        pull($alumnos, $_GET['matricula'], $_GET['asignatura'], intval($_GET['tarea']));
     }
-     
+    header("Location: ./DetalleAlumno.php?matricula=" . $_GET['matricula']);
+} else {
+    header("Location: ./Menu.php");
+}
 
-?>
+function pull($collection, $matricula, $asignatura, $numTarea)
+{
+    $updateResult = $collection->updateOne(
+        [
+            'nMatricula' => $matricula,
+            'notasAlumno.codigoAsignatura' => $asignatura
+        ],
+        ['$pull' => ['notasAlumno.$.notasAsignatura' => ['numTarea' => $numTarea]]]
+
+    );
+}
+function push($collection, $matricula, $asignatura, $numTarea, $notaTarea)
+{
+    $updateResult = $collection->updateOne(
+        [
+            'nMatricula' => $matricula,
+            'notasAlumno.codigoAsignatura' => $asignatura
+        ],
+        ['$push' => ['notasAlumno.$.notasAsignatura' => ['numTarea' => $numTarea, 'notaTarea' => $notaTarea]]]
+
+    );
+}
--- DetalleAlumno.php
+++ DetalleAlumno.php
@@ -1,82 +1,83 @@
 <!doctype html>
 <html lang="es">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Detalle Alumno</title>
-  </head>
-  <body>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<title>Detalle Alumno</title>
+</head>
+
+<body>
 	<div class="container-fluid">
 		<div class="row flex-row">
-<div class ="col-6">
+			<div class="col-6">
 
-<?php
+				<?php
 
-	if(isset($_GET['matricula'])){
-		
-		$findQuery = array('nMatricula' => $_GET['matricula']);
-		require 'vendor/autoload.php'; // incluir lo bueno de Composer
-		$cliente = new MongoDB\client;
+				if (isset($_GET['matricula'])) {
 
-		$alumnos = $cliente->ADAT_UD3_A04->alumnos;
-		$result = $alumnos->find($findQuery);
-		
-		foreach ($result as $entry) {
-    			$nombre = $entry['nombre']." ".$entry['apellido'];
-    			$matricula = $entry['nMatricula'];
-    
-    			echo "<h1> $nombre </h1>";
-    			echo "<h2> Asignaturas: </h2>";
+					$findQuery = array('nMatricula' => $_GET['matricula']);
+					require 'vendor/autoload.php'; // incluir lo bueno de Composer
+					$cliente = new MongoDB\client;
 
-    			foreach($entry['notasAlumno'] as $asig){
-        			$nombreAsig = $asig['nombreAsignatura'];
-				$codigoAsig = $asig['codigoAsignatura'];
-        			echo "<h3> &nbsp;&nbsp;&nbsp;&nbsp;".$nombreAsig."  ".$codigoAsig."</h3>";
-        			echo "<h4> &nbsp;&nbsp;&nbsp;&nbspNotas en la asignatura:</h4>";
+					$alumnos = $cliente->ADAT_UD3_A04->alumnos;
+					$result = $alumnos->find($findQuery);
 
-        			$notasAsig = $asig['notasAsignatura'];
-        			foreach($notasAsig as $asig){
-            				$num = $asig["numTarea"];
-            				$nota = $asig["notaTarea"];
+					foreach ($result as $entry) {
+						$nombre = $entry['nombre'] . " " . $entry['apellido'];
+						$matricula = $entry['nMatricula'];
 
-            				echo "<p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Numero de Tarea: ".$num." nota en la tarea ".$nota."</p>";
-       				}
-        
-        
-    			}
-		}
-	}else{
-		header("Location: ./MostrarAlumnos.php");
-	}
+						echo "<h1> $nombre </h1>";
+						echo "<h2> Asignaturas: </h2>";
 
-?>
-</div>
+						foreach ($entry['notasAlumno'] as $asig) {
+							$nombreAsig = $asig['nombreAsignatura'];
+							$codigoAsig = $asig['codigoAsignatura'];
+							echo "<h3> &nbsp;&nbsp;&nbsp;&nbsp;" . $nombreAsig . "  " . $codigoAsig . "</h3>";
+							echo "<h4> &nbsp;&nbsp;&nbsp;&nbspNotas en la asignatura:</h4>";
 
-<form class = "col-6" action="./ActualizaNotas.php">
-Matricula:<br>
-  <input type="text" name="matricula" value="<?php echo strval($_GET['matricula']); ?>" readonly="readonly">
-  <br>
-Asignatura:<br>
-  <input type="text" name="asignatura" value="Mickey">
-  <br>
-  Tarea:<br>
-  <input type="text" name="tarea" value="mick">
-  <br>
-  Nota:<br>
-  <input type="text" name="nota" value="mick">
-  <br><br>
-  <input type="submit" value="Submit">
-</form> 
+							$notasAsig = $asig['notasAsignatura'];
+							foreach ($notasAsig as $asig) {
+								$num = $asig["numTarea"];
+								$nota = $asig["notaTarea"];
+								$onClickAction = "document.location.href='./ActualizaNotas.php?tipo=borrar&matricula=" . $matricula . "&tarea=" . $num . "&asignatura=" . $codigoAsig . "'";
+
+								echo "<p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Numero de Tarea: " . $num . " nota en la tarea " . $nota . "</p>";
+								echo "<button type='button' onClick=" . $onClickAction . ">Borrar</button>";
+							}
+						}
+					}
+				} else {
+					header("Location: ./MostrarAlumnos.php");
+				}
+
+				?>
+			</div>
+
+			<form class="col-6" method="GET" action="./ActualizaNotas.php">
+				Matricula:<br>
+				<input type="text" name="matricula" value="<?php echo strval($_GET['matricula']); ?>" readonly="readonly">
+				<br>
+				Asignatura:<br>
+				<input type="text" name="asignatura" value="Mickey">
+				<br>
+				Tarea:<br>
+				<input type="text" name="tarea" value="mick">
+				<br>
+				Nota:<br>
+				<input type="text" name="nota" value="mick">
+				<br><br>
+				<input type="submit" value="Submit">
+			</form>
 
 
-</div>
-</div>
+		</div>
+	</div>
 </body>
+
 </html>
-
-
